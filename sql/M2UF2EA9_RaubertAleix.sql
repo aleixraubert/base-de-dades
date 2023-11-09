ec85b84ca04ee0bb2a037689aac1dff162ba9f32
Aleix Raubert <146190136+aleixraubert@users.noreply.github.com>
2023-11-09 10:44:36 +0100

    Update M2UF2EA9_RaubertAleix.sql

--- sql/M2UF2EA9_RaubertAleix.sql
+++ sql/M2UF2EA9_RaubertAleix.sql
@@ -1,2 +1,12 @@
 /* 1. Mostra totes les dades dels empleats que el seu lloc de feina sigui Sales Manager. */
 
+SELECT E.*
+FROM EMPLOYEES E,JOBS J
+WHERE E.JOB_ID=J.JOB_ID AND J.JOB_ID='SA_MAN';
+
+/* 2. Mostra els departaments que tinguin empleats que hagin finalitzat el seu contracte a l'any
+1998. */
+
+SELECT D.* 
+FROM DEPARTMENTS D, JOB_HISTORY J
+WHERE D.DEPARTMENT_ID=J.DEPARTMENT_ID AND END_DATE BETWEEN '1-1-1998' AND '31-12-1998';
